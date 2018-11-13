05e3588e594720b60fa475c8451434594edebb1e
MariamSarwat <37277152+MariamSarwat@users.noreply.github.com>
2018-11-12 22:20:51 -0500

    Update gestionnaireUtilisateurs.cpp

--- TD5/gestionnaireUtilisateurs.cpp
+++ TD5/gestionnaireUtilisateurs.cpp
@@ -8,54 +8,18 @@
 #include "utilisateurPremium.h"
 #include "utilisateurRegulier.h"
 
-Utilisateur* GestionnaireUtilisateurs::getUtilisateurParIndex(int i) const
-{
-	return utilisateurs_[i];
-}
+#include <algorithm>
+#include <functional>
+#include <iterator>
 
-vector<Utilisateur*> GestionnaireUtilisateurs::getUtilisateurs() const
-{
-	return utilisateurs_;
-}
+using namespace std::placeholders;
 
-int GestionnaireUtilisateurs::getNombreUtilisateurs() const
-{
-	return utilisateurs_.size();
-}
 
-int GestionnaireUtilisateurs::getIndexDe(Utilisateur * utilisateur) const
-{
-	int index = -1;
-	for (int i = 0; i < utilisateurs_.size(); i++) {
-		if (utilisateurs_[i] == utilisateur) {
-			index = i;
-			break;
-		}
-	}
-	return index;
-}
-
-GestionnaireUtilisateurs& GestionnaireUtilisateurs::ajouterUtilisateur(Utilisateur * utilisateur)
-{
-	UtilisateurRegulier* utilisateurRegulier = dynamic_cast<UtilisateurRegulier*>(utilisateur);
-	UtilisateurPremium* utilisateurPremium = dynamic_cast<UtilisateurPremium*>(utilisateur);
-
-	if (utilisateurRegulier != nullptr) {
-		if (!utilisateurRegulier->getPossedeGroupe()) {
-			utilisateurRegulier->setPossedeGroupe(true);
-		}
-		else {
-			cout << "Erreur : L'utilisateur " << utilisateur->getNom() << " n'est pas un utilisateur premium et est deja dans un groupe." << endl;
-			return *this;
-		}
-	}
-	else {
-		if (utilisateurPremium != nullptr && utilisateurPremium->getJoursRestants() <= 0) {
-			cout << "Erreur : L'utilisateur " << utilisateur->getNom() << " doit renouveler son abonnement premium" << endl;
-			return *this;
-		}
-	}
-
-	utilisateurs_.push_back(utilisateur);
-	return *this;
-}
+vector<double> GestionnaireUtilisateurs::getComptes() const {}
+int GestionnaireUtilisateurs::estExistant(Utilisateur* utilisateur) const {}
+void  GestionnaireUtilisateurs::mettreAJourComptes(Utilisateur* payePar, double montant) {}
+pair<Utilisateur*, double>&  GestionnaireUtilisateurs::getMax() const {}
+pair<Utilisateur*, double>&  GestionnaireUtilisateurs::getMin() const {}
+Utilisateur *  GestionnaireUtilisateurs::getUtilisateurSuivant(Utilisateur* utilisateur, double montant) const {}
+vector<pair<Utilisateur*, double>>  GestionnaireUtilisateurs::getUtilisateursEntre(double borneInf, double borneSup) const {}
+GestionnaireUtilisateurs&  GestionnaireUtilisateurs::setCompte(pair<Utilisateur*, double> p) {}
